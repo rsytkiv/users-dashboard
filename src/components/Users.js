fb58c6857d55931dc0160d763e6e8465215783ca
v.perozhak <v.perozhak@abtollc.com>
2021-08-10 17:10:07 +0300

    normalize all things

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../store/actions";
 
 export default function Users() {
-    const [items, setItems] = useState([]);
+  const dispatch = useDispatch();
+  const { users, loading } = useSelector((state) => ({
+    users: state.users.users,
+    loading: state.users.loading,
+  }));
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setItems(result);
-                console.log(result);
-            },
-        )
-    }, []);
-    
-    return (
-      <div className="container">
-        <table>
-            {items.length && items.map((item) => 
-                <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.name}</td>
-                    <td>{item.username}</td>
-                </tr>
-            )}
-        </table>
-        <button>Posts</button>
-      </div>
+  let list = <h2 className="mt-5 ml-5">List is empty</h2>;
+
+  if (loading) list = <h2 className="mt-5 ml-5">Loading..</h2>;
+
+  if (users?.length)
+    list = (
+      <table>
+        {users?.length &&
+          users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+            </tr>
+          ))}
+      </table>
     );
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      {list}
+      <button>Posts</button>
+    </div>
+  );
 }
