20d65dbfe78154a3ff8c3982765bdaae0c14e8e0
v.perozhak <v.perozhak@abtollc.com>
2021-08-15 17:20:06 +0300

    remove materialize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-import 'materialize-css'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import store from "./store";
